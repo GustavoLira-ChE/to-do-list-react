a13fbececfd1ec6f6d3ab91ec7e208c47dfe5fd8
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-27 15:03:37 -0500

    add: loaders and plugins for js, jsx, html and css, style, postcss, sass

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -8,10 +10,64 @@ module.exports = {
         clean: true,
     },
     resolve: {
-        extensions: [".js", "jsx"]
+        extensions: [".js", ".jsx"]
     },
+    mode: "production",
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ],
+            },
+            {
+                test: /\.s?[ac]?ss$/i,
+                use: [{
+                    // inject CSS to page
+                    loader: 'style-loader'
+                  }, {
+                    // translates CSS into CommonJS modules
+                    loader: 'css-loader'
+                  }, {
+                    // compiles Sass to CSS
+                    loader: 'sass-loader'
+                  }, {
+                    // Run postcss actions
+                    loader: 'postcss-loader',
+                    options: {
+                      // `postcssOptions` is needed for postcss 8.x;
+                      // if you use postcss 7.x skip the key
+                      postcssOptions: {
+                        // postcss plugins, can be exported to postcss.config.js
+                        plugins: () => {
+                          [
+                            require('autoprefixer')
+                          ];
+                        }
+                      }
+                    }
+                  }, 
+                ]
+            },
+        ],
     },
-    plugins: [],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+    ],
 };
