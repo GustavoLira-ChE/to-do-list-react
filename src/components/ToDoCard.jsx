5788f946fb21e25cc879e036a08620b78f729bd4
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-28 16:17:35 -0500

    add: todo card as a template to display todo list items

---
+++ src/components/ToDoCard.jsx
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+import ToDoContext from '../context/ToDoContext';
+
+const ToDoCard = ({ toDo }) => {
+
+    const { deleteToDoItem, changeToDoState } = useContext(ToDoContext);
+
+    return (
+            <div className='card mt-2'>
+                <div className='card-body'>
+                    <h3 className='card-title'>
+                        {toDo.title}
+                        <button className={`btn btn-sm ${toDo.status ? "btn-success" : "btn-outline-success"} m-2`} onClick={()=>changeToDoState(toDo.id)}>{toDo.status ? "Finished" : "Finish"}</button>
+                    </h3>
+                    <p className='card-text'>
+                        {toDo.descr}
+                    </p>
+                    <hr />
+                    <div className='d-flex justify-content-end'>
+                        <button className='btn btn-sm btn-outline-primary me-2'>
+                            Edit
+                        </button>
+                        <button className='btn btn-sm btn-outline-danger me-2' onClick={()=> deleteToDoItem(toDo.id)}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default ToDoCard;
