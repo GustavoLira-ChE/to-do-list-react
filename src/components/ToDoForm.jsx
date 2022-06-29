379b948b7d563f2c2bed553bf55a25fd713d32ea
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-29 16:15:16 -0500

    feat: add a button to clear input and textarea

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -16,27 +16,41 @@ const ToDoForm = () => {
             status: false
         }
         addNewToDoItem(data);
+    };
+
+    const handleTextArea = (event) => {
+        event.preventDefault();
+        document.getElementById("input-id").value = ""
+        document.getElementById("input-description").value = ""
     }
     return (
         <div>
             <h1>New Task</h1>
             <form ref={form}>
                 <input 
+                    id="input-id"
                     type="text"  
                     placeholder="Title" className="form-control"
                     name="title" 
                     />
                 <textarea 
+                    id="input-description"
                     placeholder="Description" 
                     className="form-control mt-2"
                     name="description" 
                     cols="30" 
                     rows="10"
                     ></textarea>
-                <button 
-                    className="btn btn-primary btn-block mt-2"
-                    onClick={handleFormValues}
-                    >Add Task</button>
+                <div className="d-flex justify-content-between">
+                    <button 
+                        className="btn btn-primary btn-block mt-2"
+                        onClick={handleFormValues}
+                        >Add Task</button>
+                    <button
+                        className="btn btn-danger btn-block mt-2"
+                        onClick={handleTextArea}
+                    >Clear Text</button>
+                </div>
             </form>
         </div>
     );
